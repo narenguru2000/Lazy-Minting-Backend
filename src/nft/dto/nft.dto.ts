845253d1c4fe4e651987389d8d1d6cf0ef01bcb0
Narendraguru Harikrishnan <narendraguru.h@rejolut.com>
2023-02-27 20:54:46 +0530

    feat(swagger): addedswagger docs.

--- src/nft/dto/nft.dto.ts
+++ src/nft/dto/nft.dto.ts
@@ -1,16 +1,24 @@
 import { Type } from '@nestjs/class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GetAllnftResposne {
+  @ApiProperty()
   tokenId: number;
+
+  @ApiProperty()
   uri: string;
+
+  @ApiProperty()
   minPrice: number;
 }
 
 export class storeNftVoucher extends GetAllnftResposne {
+  @ApiProperty()
   signature: string;
 }
 
 export class getUserNftReq {
+  @ApiProperty({ example: 1 })
   @Type(() => Number)
   tokenId: number;
 }
